7e2ea3ff560fb6f2b09429b964e6f81a1e6a6e8b
oğulcan yetim <64482227+oqullcan@users.noreply.github.com>
2026-04-09 14:02:45 +0300

    .

--- albus.ps1
+++ albus.ps1
@@ -1578,48 +1578,94 @@ if (Test-Path $SettingsDat) {
     }
 }
 
-
-# # power & performance
+# power 
 Status "deploying albus core power policy..." "step"
-$AlbusGUID = "a1b050f1-c0de-4a1b-9cac-f1ce7c7c7c7c"
+$PowerSaverGUID = "a1841308-3541-4fab-bc81-f71556f20b4a"
+
+& powercfg -restoredefaultschemes 2>&1 | Out-Null
+& powercfg /SETACTIVE $PowerSaverGUID 2>&1 | Out-Null
+
+$AlbusGUID = ""
+$duplicateOutput = & powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+if ($duplicateOutput -match '([0-9a-fA-F]{8}(-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12})') {
+    $AlbusGUID = $Matches[1]
+} else {
+    $AlbusGUID = "99999999-9999-9999-9999-999999999999"
+    & powercfg /delete $AlbusGUID 2>&1 | Out-Null
+    & powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 $AlbusGUID 2>&1 | Out-Null
+}
+
+& powercfg /changename $AlbusGUID "Albus" "optimized for minimal latency, deep unparking, and peak hardware throughput." 2>&1 | Out-Null
+
 $PList = & powercfg /l 2>$null | Out-String
 
-# 1. structure container
-if ($PList -notmatch $AlbusGUID) {
-    $Src = @("e9a42b02-d5df-448d-aa00-03f14749eb61","8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c","381b4222-f694-41f0-9685-ff5bb260df2e") | Where-Object { $PList -match $_ } | Select-Object -First 1
-    if ($Src) { & { powercfg /duplicatescheme $Src $AlbusGUID } 2>$null | Out-Null }
+if (-not [string]::IsNullOrEmpty($PList)) {
+    $PList -split "`r?`n" | ForEach-Object {
+        if ($_ -match ':') {
+            $parse = $_ -split ':'
+            if ($parse.Count -gt 1) {
+                $index = $parse[1].Trim().IndexOf('(')
+                if ($index -gt 0) {
+                    $guid = $parse[1].Trim().Substring(0, $index).Trim()
+                    if ($guid -ne $AlbusGUID -and $guid -ne $PowerSaverGUID -and $guid.Length -ge 36) {
+                        & powercfg /delete $guid 2>&1 | Out-Null
+                    }
+                }
+            }
+        }
+    }
 }
 
-# 2. metadata injection
-$PowerReg = "HKLM:\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\$AlbusGUID"
-if (Test-Path $PowerReg) {
-    Set-ItemProperty -Path $PowerReg -Name "FriendlyName" -Value "Albus Power Scheme" -Force -ErrorAction SilentlyContinue
-    Set-ItemProperty -Path $PowerReg -Name "Description" -Value "optimized for low-latency and peak hardware performance by albus engine." -Force -ErrorAction SilentlyContinue
-}
-& { powercfg /setactive $AlbusGUID } 2>$null | Out-Null
-
-# 3. native low-latency hardware tweaks
 @(
-    "0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0", # disk
-    "19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 0", # wifi
-    "501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 0", # pci-e
-    "238c9fa8-0aad-41ed-83f4-97be242c8f20 29f6c1db-86da-48c5-9fdb-f2b67b1f44da 0", # sleep
-    "54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 100", # cpu min
-    "54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100", # cpu max
-    "54533251-82be-4824-96c1-47b60b740d00 893dee03-5242-4997-a44d-ef36649442f1 1",   # boost
-    "2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0"    # usb
+    "0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0",    # hard disk turn off
+    "0d7dbae2-4294-402a-ba8e-26777e8488cd 309dce9b-bef4-4119-9921-a851fb12f0f4 1",    # desktop background slideshow
+    "19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 0",    # wireless adapter
+    "238c9fa8-0aad-41ed-83f4-97be242c8f20 29f6c1db-86da-48c5-9fdb-f2b67b1f44da 0",    # sleep after
+    "238c9fa8-0aad-41ed-83f4-97be242c8f20 94ac6d29-73ce-41a6-809f-6363ba21b47e 0",    # allow hybrid sleep off
+    "238c9fa8-0aad-41ed-83f4-97be242c8f20 9d7815a6-7ee4-497e-8888-515a05f02364 0",    # hibernate after
+    "238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d 0",    # allow wake timers disable
+    "2a737441-1930-4402-8d77-b2bebba308a3 0853a681-27c8-4100-a2fd-82013e970683 0",    # usb hub selective suspend timeout
+    "2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0",    # usb selective suspend setting disabled
+    "2a737441-1930-4402-8d77-b2bebba308a3 d4e98f31-5ffe-4ce1-be31-1b38b384c009 0",    # usb 3 link power management off
+    "4f971e89-eebd-4455-a8de-9e59040e7347 a7066653-8d6c-40a8-910e-a1f54b84c7e5 2",    # start menu power button shut down
+    "501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 0",    # pci express link state pm off
+    "54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 100",  # min processor state
+    "54533251-82be-4824-96c1-47b60b740d00 94d3a615-a899-4ac5-ae2b-e4d8f634367f 1",    # system cooling active
+    "54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100",  # max processor state
+    "54533251-82be-4824-96c1-47b60b740d00 0cc5b647-c1df-4637-891a-dec35c318583 100",  # unpark cpu cores min
+    "54533251-82be-4824-96c1-47b60b740d00 ea062031-0e34-4ff1-9b6d-eb1059334028 100",  # unpark cpu cores max
+    "54533251-82be-4824-96c1-47b60b740d00 36687f9e-e3a5-4dbf-b1dc-15eb381c6863 0",    # cpu energy performance preference
+    "54533251-82be-4824-96c1-47b60b740d00 93b8b6dc-0698-4d1c-9ee4-0644e900c85d 0",    # heterogeneous thread scheduling
+    "54533251-82be-4824-96c1-47b60b740d00 75b0ae3f-bce0-45a7-8c89-c9611c25e100 0",    # lock maximum processor frequency
+    "7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 600",  # display timeout 10 min
+    "7516b95f-f776-4464-8c53-06167f40cc99 aded5e82-b909-4619-9949-f5d71dac0bcb 100",  # display brightness
+    "7516b95f-f776-4464-8c53-06167f40cc99 f1fbfde2-a960-4165-9f88-50667911ce96 100",  # dimmed brightness
+    "7516b95f-f776-4464-8c53-06167f40cc99 fbd9aa66-9553-4097-ba44-ed6e9d65eab8 0",    # adaptive brightness off
+    "9596fb26-9850-41fd-ac3e-f7c3c00afd4b 10778347-1370-4ee0-8bbd-33bdacaade49 1",    # video playback bias
+    "9596fb26-9850-41fd-ac3e-f7c3c00afd4b 34c7b99f-9a6d-4b3c-8dc7-b6693b78cef4 0",    # optimize video quality
+    "e276e160-7cb0-43c6-b20b-73f5dce39954 a1662ab2-9d34-4e53-ba8b-2639b9e20857 3",    # switchable graphics global maximize
+    "e73a048d-bf27-4f12-9731-8b2076e8891f 5dbb7c9f-38e9-40d2-9749-4f8a0e9f640f 0",    # critical battery notification
+    "e73a048d-bf27-4f12-9731-8b2076e8891f 637ea02f-bbcb-4015-8e2c-a1c7b9c0b546 0",    # critical battery action
+    "e73a048d-bf27-4f12-9731-8b2076e8891f 8183ba9a-e910-48da-8769-14ae6dc1170a 0",    # low battery level
+    "e73a048d-bf27-4f12-9731-8b2076e8891f 9a66d8d7-4ff7-4ef9-b5a2-5a326ca2a469 0",    # critical battery level
+    "e73a048d-bf27-4f12-9731-8b2076e8891f bcded951-187b-4d05-bccc-f7e51960c258 0",    # low battery notification
+    "e73a048d-bf27-4f12-9731-8b2076e8891f d8742dcb-3e6a-4b3c-b3fe-374623cdcf06 0",    # low battery action
+    "e73a048d-bf27-4f12-9731-8b2076e8891f f3c5027d-cd16-4930-aa6b-90db844a8f00 0",    # reserve battery level
+    "de830923-a562-41af-a086-e3a2c6bad2da 13d09884-f74e-474a-a852-b6bde8ad03a8 100",  # low screen brightness battery saver
+    "de830923-a562-41af-a086-e3a2c6bad2da e69653ca-cf7f-4f05-aa73-cb833fa90ad4 0"     # turn battery saver on automatically
 ) | ForEach-Object {
     if ($_ -match '(?<s>[a-f0-9-]+)\s+(?<i>[a-f0-9-]+)\s+(?<v>\d+)') {
         $s = $Matches.s; $i = $Matches.i; $v = $Matches.v
+        & powercfg /attributes $s $i -ATTRIB_HIDE 2>$null | Out-Null
         & { 
             trap { continue }
-            powercfg /attributes $s $i -ATTRIB_HIDE 2>$null | Out-Null
             powercfg /setacvalueindex $AlbusGUID $s $i $v 2>$null | Out-Null
             powercfg /setdcvalueindex $AlbusGUID $s $i $v 2>$null | Out-Null
         }
     }
 }
-& { powercfg /setactive $AlbusGUID } 2>$null | Out-Null
+
+& powercfg /SETACTIVE $AlbusGUID 2>&1 | Out-Null
 
 # 4. global performance tweaks
 & { powercfg /hibernate off } 2>$null | Out-Null
