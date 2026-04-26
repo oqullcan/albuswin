f36e4c91693b8e362cf7867526a7a749aee77a1a
oğulcan yetim <64482227+oqullcan@users.noreply.github.com>
2026-04-26 23:09:24 +0300

    .

---
+++ albus-minimalism.ps1
@@ -0,0 +1,10 @@
+# ───────────────────────────────────────────────────────────
+#                albus playbook - minimalism
+# ───────────────────────────────────────────────────────────
+
+$ErrorActionPreference = 'Stop'
+$ProgressPreference    = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+...
